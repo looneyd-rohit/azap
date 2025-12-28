cd8682837e4d471fc69b88484804e4d2afce3d89
ROHIT SADHU <rohitsadhupersonal@gmail.com>
2025-12-28 19:04:51 +0530

    works till now

--- types.ts
+++ types.ts
@@ -2,6 +2,7 @@ import {Server as NetServer, Socket} from 'net';
 import {NextApiResponse} from 'next';
 import {Server as SocketIOServer} from 'socket.io';
 
+declare module "*.css";
 
 export type NextApiResponseServerIo = NextApiResponse & {
     socket: Socket & {
